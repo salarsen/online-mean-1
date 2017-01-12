fd2d933c412127a33465d9133660258885ec1495
Litton Chen <litton@bloomboard.com>
2017-01-11 16:25:47 -0800

    Littons Attempt at 1/3 challenge

--- 01-03-objects/main.js
+++ 01-03-objects/main.js
@@ -1,28 +1,39 @@
 function personCreator(name, items) {
-    return { 
+    return {
         name,
         items,
-        stealFrom: function(){}
+        stealFrom: function(victim, item){
+          for(var i = 0; i < victim.items.length; i++){
+              if(victim.items[i] === item){
+                  items.push(item);
+                  victim.items.splice(i,1);
+                  return true;
+              };
+          };
+          return false;
+        }
     };
 }
 
-function stealAnItem(stealer,jason,item){
-    for(var i = 0; i < jason.items.length; i++){
-        console.log('For Loop',i,jason.items);
-        if(jason.items[i] === item){
-            stealer.items.push(item)
-            jason.items.splice(i,1);
-            return true;
-        };
-    };
-    return false;
-}
+// function stealAnItem(stealer,jason,item){
+//     for(var i = 0; i < jason.items.length; i++){
+//         console.log('For Loop',i,jason.items);
+//         if(jason.items[i] === item){
+//             stealer.items.push(item)
+//             jason.items.splice(i,1);
+//             return true;
+//         };
+//     };
+//     return false;
+// }
 
-var charlie = personCreator('Charlie', ['wallet', 'gtkeys']);
+var charlie = personCreator('Charlie', ['wallet', 'keys']);
 var jason = personCreator('Jason', ['chapstick', 'keys', 'phone']);
 
 // Get this to work!
-/*
+
 charlie.stealFrom(jason, 'phone');
 jason.stealFrom(charlie, 'wallet');
-*/
+
+console.log(charlie);
+console.log(jason);
