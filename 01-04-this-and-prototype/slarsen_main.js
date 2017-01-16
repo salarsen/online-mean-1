10c1e1dae507e500c7f399a28abfe8ab1926c2d1
salarsen <larsen.spencer@gmail.com>
2017-01-15 16:34:25 -0800

    Slarsen object prototype

---
+++ 01-04-this-and-prototype/slarsen_main.js
@@ -0,0 +1,25 @@
+var personPrototype = {
+    stealFrom: function(victim,item){
+        for(var i = 0; i < victim.ownedItems.length; i++){
+            if (victim.ownedItems[i] === item){
+                victim.ownedItems.splice(i,1);
+                this.ownedItems.push(item); // could do `${victim.name} ${item}` to indicate if someone has someone elses property
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+function personCreator(name,items){
+    var personObj = Object.create(personPrototype);
+    personObj.name = name;
+    personObj.ownedItems = items;
+    return personObj;
+}
+
+var spencer = personCreator('Spencer',['wallet','keys']);
+var julia = personCreator('Julia',['chapstick','keys','phone']);
+
+console.log(spencer);
+console.log(julia);
