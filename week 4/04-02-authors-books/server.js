1f5bc19966fdb341c41cd94caf7dbd9e7e645ae7
Spencer Larsen <larsen.spener@gmail.com>
2017-02-04 16:29:25 -0800

    Revert "fixing git stuff"
    This reverts commit 37f81d6413c1d6a2d33c1a946ad2c7d35be1a758, reversing
    changes made to 0ede72196e669f4c335b54644036773008457f7d.
    
    	renamed:    week 4/04-02-spencerLarsen/package.json -> week 4/04-02-authors-books/package.json
    	renamed:    week 4/04-02-spencerLarsen/server.js -> week 4/04-02-authors-books/server.js
    	renamed:    week 4/04-02-spencerLarsen/views/authors/index.ejs -> week 4/04-02-authors-books/views/authors/index.ejs
    	new file:   week 4/04-02-authors-books/views/authors/new.ejs
    	renamed:    week 4/04-02-spencerLarsen/views/authors/show.ejs -> week 4/04-02-authors-books/views/authors/show.ejs
    	renamed:    week 4/04-02-spencerLarsen/views/books/index.ejs -> week 4/04-02-authors-books/views/books/index.ejs
    	new file:   week 4/04-02-authors-books/views/books/new.ejs
    	renamed:    week 4/04-02-spencerLarsen/views/books/show.ejs -> week 4/04-02-authors-books/views/books/show.ejs
    	renamed:    week 4/04-02-spencerLarsen/views/index.ejs -> week 4/04-02-authors-books/views/index.ejs
    	deleted:    week 4/04-02-spencerLarsen/views/authors/new.ejs
    	deleted:    week 4/04-02-spencerLarsen/views/books/new.ejs

--- week 4/04-02-authors-books/server.js
+++ week 4/04-02-authors-books/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const parser = require('body-parser');
-<<<<<<< HEAD
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
@@ -30,48 +29,10 @@ app.set('views',path.resolve('views'));
 //     return new Schema;
 //   }
 // }
-=======
-const port = process.env.PORT || 9898;
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const app = express();
-
-app.use(express.static(path.resolve('client')));
-app.use(parser.urlencoded({ extended: true }));
-app.use(parser.json());
-
-app.set('view engine', 'ejs');
-app.set('views', path.resolve('views'));
-
-mongoose.Promise = global.Promise;
-
-mongoose.connect('mongodb://localhost/authors_books_db');
-
-mongoose.connection.on('connected', function() {
-  console.log('connected to mongoose');
-});
-
-/*
-  Author:
-    name: String
-    age: Number
-    isAlive: Boolean
-    books: Book[]
-
-
-  Book
-    title: String
-    publishDate: Date
-    pageCount: Number
-    subject: String
-    author: Author
-*/
->>>>>>> 07e2b3d17631d3a0448815c7b8fdd6161d3f4a0c
 
 const AuthorSchema = new Schema({
   name: String,
   age: Number,
-<<<<<<< HEAD
   isAlive: Boolean,
   /*
    {
@@ -89,23 +50,6 @@ const BookSchema = new Schema({
   title: String,
   publishDate: Date,
   pageCount: Number,
-=======
-  isAlive: {
-    type: Boolean,
-    default: false
-  },
-  books: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Book'
-  }]
-});
-
-
-const BookSchema = new Schema({
-  title: String,
-  publishDate: Date,
-  pagecount: Number,
->>>>>>> 07e2b3d17631d3a0448815c7b8fdd6161d3f4a0c
   subject: String,
   author: {
     type: Schema.Types.ObjectId,
@@ -113,7 +57,6 @@ const BookSchema = new Schema({
   }
 });
 
-<<<<<<< HEAD
 //module.exports =
 const Author = mongoose.model('Author',AuthorSchema);
 const Book = mongoose.model('Book',BookSchema);
@@ -236,103 +179,4 @@ function handleError(response,error){
 
 app.listen(port,function(){
   console.log(`Server listening on port: ${port}`);
-=======
-const Author = mongoose.model('Author', AuthorSchema);
-const Book = mongoose.model('Book', BookSchema);
-
-app.get('/', function(request, response) {
-  response.render('index');
-})
-.get('/authors/new', function(request, response) {
-  response.render('authors/new')
-})
-.get('/authors', function(request, response) {
-  Author.find({})
-    .then(function(authors) {
-      response.render('authors/index', { authors });
-    })
-    .catch(function(error) {
-      response.json(error);
-    })
-})
-.get('/authors/:id', function(request, response) {
-  console.log(request.params);
-  Author.findById(request.params.id)
-    .populate('books')
-    .exec()
-    .then(function(author) {
-      response.render('authors/show', { author });
-    })
-    .catch(function(error) {
-      response.json(error);
-    })
-})
-.post('/authors/create', function(request, response) {
-  console.log(request.body);
-
-  request.body.isAlive = !!request.body.isAlive;
-
-  Author.create(request.body)
-    .then(function(user) {
-      response.redirect('/authors');
-    })
-    .catch(function(error) {
-      response.json(error);
-    })
-})
-
-
-
-
-
-
-.get('/books', function(request, response) {
-  Book.find({})
-    .populate('author')
-    .exec()
-    .then(function(books) {
-      response.render('books/index', { books });
-    })
-    .catch(function(error) {
-      response.json(error);
-    })
-})
-.get('/books/new', function(request, response) {
-  Author.find({})
-    .then(function(authors) {
-      response.render('books/new', { authors });
-    })
-    .catch(function(error) {
-      handleError(response, error);
-    })
-})
-.post('/books/create', function(request, response) {
-  console.log(request.body);
-
-  Book.create(request.body)
-    .then(function(book) {
-      return Author.findById(book.author)
-        .then(function(author) {
-          author.books.push(book);
-
-          return author.save()
-            .then(function(...args) {
-              response.redirect('/books');
-            });
-        })
-    })
-    .catch(function(error) {
-      response.json(error);
-    });
-})
-
-
-function handleError(response, error) {
-  // log the error
-  response.status(500).json(error);
-}
-
-app.listen(port, function() {
-  console.log(`server listening on port ${port}`);
->>>>>>> 07e2b3d17631d3a0448815c7b8fdd6161d3f4a0c
 });
